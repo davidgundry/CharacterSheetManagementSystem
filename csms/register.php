48ecf9c4b5138c740dfd5c1fb180d2543a7571ad
davidgundry <davidgundry@gmail.com>
2014-09-02 09:53:41 +0100

    Fixing registration and changing settings

--- csms/register.php
+++ csms/register.php
@@ -2,19 +2,40 @@
   $PAGENAME = "Register";
   $PAGETITLE= "Register an account";
   include "../func.inc.php";
-  include "../session.php";
   include "../header.php";
-  include "../menu.php";
 
   /* Need to connect to db for sanitization function to work.
      Then sanitize username and email or die with an error. */
   include "../dbconnect.php";
   if (function_exists('sanitize'))
   {
+      if ((isset($_POST['username'])) and (isset($_POST['email'])) and (isset($_POST['password'])) and isset($_POST['password-c']))
+      {
 	  $user = sanitize($_POST['username']);
 	  $email = sanitize($_POST['email']);
 	  $pass = $_POST['password'];
 	  $passcon = $_POST['password-c'];
+	  
+	  if ($user == '')
+	  {
+		print "<div class='error'><span class='title'>No username</span>No username was suppplied.</div>";
+	  }
+	  else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+	  {
+		print "<div class='error'><span class='title'>Invalid Email</span>The email address supplied is invalid.</div>";
+	  }
+	  else if ($pass == $passcon)	
+	  {
+	  	  $pass = md5($pass);
+		  $query = "INSERT INTO `users` (`username` ,`password` ,`email` ,`active` ) VALUES ('$user', '$pass', '$email', '0');";
+		  $result = mysql_query($query);
+		  print "<div class='notification'><span class='title'>Submitted for approval</span>The new user account has been submited for adminstrator approval.</p><p>Go to <a href='index.php'>Login</a> page.</div>";
+	  }
+	  else
+	  {
+		print "<p>The two passwords did not match.</p.>";
+	  }
+      }
   }
   else
   {
@@ -35,28 +56,8 @@
 				<input type='submit' value='Register' />
 			</p>
 		</form>
-
-	<?php 
-	  if ($user == '')
-	  {
-		print "<div class='error'><span class='title'>No username</span>No username was suppplied.</div>";
-	  }
-	  else if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-	  {
-		print "<div class='error'><span class='title'>Invalid Email</span>The email address supplied is invalid.</div>";
-	  }
-	  else if ($pass == $passcon)	
-	  {
-	  	  $pass = md5($pass);
-		  $query = "INSERT INTO `users` (`username` ,`password` ,`email` ,`active` ) VALUES ('$user', '$pass', '$email', '0');";
-		  $result = mysql_query($query);
-		  print "<div class='notification'><span class='title'>Submitted for approval</span>The new user account has been submited for adminstrator approval.</p><p>Go to <a href='index.php'>Login</a> page.</div>";
-	  }
-	  else
-	  {
-		print "<p>The two passwords did not match.</p.>";
-	  }
-	?>
+		
+		<p>Return to <a href="index.php">Login</a> page.</p>
 </div>
 
 <?php
--- csms/settings.php
+++ csms/settings.php
@@ -13,19 +13,15 @@
 		header("location:?");
 	}
 
-	include "../header.php";
-	include "../menu.php";
-?>
-<p>Registered on <span class='time'><?php echo $user['registration-date']; ?></span></p>
-<?php
-
+	$error = 0;
+	
 	/* If the player has submited a changed email address. */
 	if (isset($_POST['email']))
 	{	
 		$email = $_POST['email'];
-		if (filter_var($email, FILTER_VALIDATE_EMAIL))
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 	  	{
-			print "<div class='error'><span class='title>Invalid email address</span>The email address supplied is invalid.</div>";
+			$error = 1;
 	  	}
 		/* Sanitize new email and update the database. */
 		else if (function_exists('sanitize'))
@@ -33,13 +29,17 @@
 			$newEmail = sanitize($_POST['email']);
 			$query = "UPDATE users SET email='".$newEmail."' WHERE uid='". $user['uid']. "' ";
 			$result = mysql_query($query);
+			header("location:?es=1");
 		}
 		else
+		{
 			print "<div class='error'><span class='title'>Server Error</span></div>";
+		}
 	}
 	
 	/* If the player has submited a new password */
 	if ((isset($_POST['newpass'])) and (isset($_POST['oldpass'])) and isset($_POST['newpassc']))
+	{
 		if (($_POST['newpass'] != '') and ($_POST['newpass'] == $_POST['newpassc']))
 		{
 			$oldpass = md5($_POST['oldpass']);
@@ -50,36 +50,61 @@
 				$newpass = md5($_POST['newpass']);
 				$query = "UPDATE users SET password='".$newpass."' WHERE uid='". $user['uid']. "' ";
 				$result = mysql_query($query);
-				echo "<div class='notification'><span class='title'>Password changed</span>Your password has been updated.</div>";
+				header("location:?ps=1");
 
 			} else
-				echo "<div class='error'><span class='title'>Invalid password</span>Your old password was not recognised.</div>";
+				$error = 2;
 		}
 		else
-			echo "<div class='error'><span class='title'>Details were not changed</span>Your password has not been updated. Either you did not enter a new password, or the passwords you entered did not match.</div>";
-
-	if (function_exists('sanitize'))
-	{
-		$_SESSION = sanitize($_SESSION);
-		$result = mysql_query("SELECT * FROM users WHERE uid = '" . $_SESSION['user'] . "'");
-		$user = sanitize(mysql_fetch_array($result));
+			$error = 3;
 	}
-	else
-		print "<div class='error'><span class='title'>Server Error</span></div>";
+	
+	//if (function_exists('sanitize'))
+	//{
+	//	$_SESSION = sanitize($_SESSION);
+	//	$result = mysql_query("SELECT * FROM users WHERE uid = '" . $_SESSION['user'] . "'");
+	//	$user = sanitize(mysql_fetch_array($result));
+	//}
+	//else
+	//{
+	//	print "<div class='error'><span class='title'>Server Error</span></div>";
+	//	die();
+	//}
+	
+	include "../header.php";
+	include "../menu.php";
+	
+	if ($error == 1)
+		echo "<div class='error'><span class='title'>Invalid email address</span>The email address supplied is invalid.</div>";
+	else if ($error == 2)
+		echo "<div class='error'><span class='title'>Invalid password</span>Your old password was not recognised.</div>";
+	else if ($error == 3)
+		echo "<div class='error'><span class='title'>Details were not changed</span>Your password has not been updated. Either you did not enter a new password, or the passwords you entered did not match.</div>";
+	
+	if (isset($_GET['es']))
+	{
+	  echo "<div class='notification'><span class='title'>Email changed</span>Your email address has been updated.</div>";
+	}
+	if (isset($_GET['ps']))
+	{
+	  echo "<div class='notification'><span class='title'>Password changed</span>Your password has been updated.</div>";
+	}
 ?>
-
+<p>Registered on <span class='time'><?php echo $user['registration-date']; ?></span></p>
 		<form action="settings.php" method="post">
-
 			<p>Username: <?php echo $user['username']; ?><br />
 			<label for="email">Email:</label> <input id="email" name="email" type="text" value="<?php echo $user['email']; ?>" /></p>
+			<p><input type="submit" value="Update" /></p>
+		</form>
+
+		<form action="settings.php" method="post">
 			<p><label for="oldpass">Current Password:</label> <input id="oldpass" name="oldpass" type="password" value="" /><br />
 			<label for="newpass">New Password:</label> <input id="newpass" name="newpass" type="password" value="" /><br />
 			<label for="newpassc">New Password Confirm:</label> <input id="newpassc" name="newpassc" type="password" value="" /></p>
 
 			<p><input type="submit" value="Update" /></p>
-
-			<p>Images: <a href="?images=0">Off</a> <a href="?images=1">16x16</a> <a href="?images=2">32x32</a> <a href="?images=3">48x48</a></p>
-
 		</form>
+		
+		<p>Images: <a href="?images=0">Off</a> <a href="?images=1">16x16</a> <a href="?images=2">32x32</a> <a href="?images=3">48x48</a></p>
 	</div>
 <?php include "../footer.php"; ?>
